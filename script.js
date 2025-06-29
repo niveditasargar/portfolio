66c34e6f526d0ae25b224033a152f75345a9cffb
niveditasargar <165678103+niveditasargar@users.noreply.github.com>
2025-06-29 11:42:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttons = document.querySelectorAll('.product-card button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Product added to cart!');
+    });
+});
